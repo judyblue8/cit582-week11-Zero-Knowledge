8b7322e44fd2500344c9406414cce99a56299c6e
judyblue8 <74081546+judyblue8@users.noreply.github.com>
2022-03-31 18:49:55 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -8,5 +8,15 @@ def ZK_equality(G,H):
     #Generate a NIZK proving equality of the plaintexts
 
     #Return two ciphertexts and the proof
+
+    message=Secret(utils.get_random_num(bits=128))
+    R1 = Secret(utils.get_random_num(bits=128))
+    R2 = Secret(utils.get_random_num(bits=128))
+
+    C1,C2=R1.value*G,R1.value*H+message.value*G
+    D1,D2=R2.value*G,R2.value*H+message.value*G
+    skmt=DLRep(C1,R1*G)&DLRep(C2,R1*H+message*G)&DLRep(D1,R2*G)&DLRep(D2,R2*H+message*G)
+    zk_proof=skmt.prove()
+
     return (C1,C2), (D1,D2), zk_proof
 
